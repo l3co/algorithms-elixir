4838f0e5656159dddb5ecd4c130efba0fd12ae8c
Leandro Costa <lcosta.sp.br@gmail.com>
2022-11-25 08:32:32 -0300

    feat(init): start a new project with mix

---
+++ lib/algorithms_elixir.ex
@@ -0,0 +1,18 @@
+defmodule AlgorithmsElixir do
+  @moduledoc """
+  Documentation for `AlgorithmsElixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AlgorithmsElixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/algorithms_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule AlgorithmsElixirTest do
+  use ExUnit.Case
+  doctest AlgorithmsElixir
+
+  test "greets the world" do
+    assert AlgorithmsElixir.hello() == :world
+  end
+end
